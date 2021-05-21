25f8eee9509bcad2e799f16cc5e357791192da7b
jacka03 <758168660@qq.com>
2021-05-21 09:57:05 +0800

    add analysis

--- analysis/tool/analysis.py
+++ analysis/tool/analysis.py
@@ -2,25 +2,36 @@ import nupack
 from nupack import *
 
 
+def analysis_all(gene_list1, gene_list2):
+    count = len(gene_list1) + len(gene_list2)
+    name = 65
+    dict = {}
+    for strand in gene_list1:
+        dict[Strand(strand, name=chr(name))] = 1e-8
+        name+=1
+
+    for strand in gene_list2:
+        dict[Strand(strand, name=chr(name))] = 1e-8
+        name+=1
 
 
+    # print(dict)
 
-def analysis():
-    """
-
-    """
     my_model = Model(material='dna', celsius=37)
-    A = Strand('TGAGATTTAGTTCAACGGATATGCAATACCAAAAGATGCTATTTGCTGCTATTC', name='A')
-    B = Strand('TTTGTGCATTAAGTTCGAAGAAGATCTCAATCTATAATGAAGAAATGATAGTAGCTGGTTGTTTTATAGGCT', name='B')
-    C = Strand('AGATTGAGATCTTCTTCGAACTTAATGCACAAATAAATAGAATAGCAGCAAATAGCATCTTTTGGT', name='C')
-    D = Strand('ACCTAAACTCTTCCGACTGAATATGAGAAAGCCTATAAAACAACCAGCTACTATCATTTCTTC', name='D')
+    # A = Strand('TGAGATTTAGTTCAACGGATATGCAATACCAAAAGATGCTATTTGCTGCTATTC', name='A')
+    # B = Strand('TTTGTGCATTAAGTTCGAAGAAGATCTCAATCTATAATGAAGAAATGATAGTAGCTGGTTGTTTTATAGGCT', name='B')
+    # C = Strand('AGATTGAGATCTTCTTCGAACTTAATGCACAAATAAATAGAATAGCAGCAAATAGCATCTTTTGGT', name='C')
+    # D = Strand('ACCTAAACTCTTCCGACTGAATATGAGAAAGCCTATAAAACAACCAGCTACTATCATTTCTTC', name='D')
 
     # t1 = Tube(strands={a: 1e-8, b: 1e-9, c: 1e-8, d: 1e-9}, complexes=SetSpec(max_size=4), name='t1')
     # tube_results = tube_analysis(tubes=[t1, t2], model=model1)
 
-    t1 = Tube(strands={A: 1e-8, D: 1e-8}, complexes=SetSpec(max_size=2), name='t1')  # complexes defaults to [A, B]
-    print(t1.complexes)
-    tube_results = tube_analysis(tubes=[t1], model=my_model)
+    t1 = Tube(strands=dict, complexes=SetSpec(max_size=count), name='t1')  # complexes defaults to [A, B]
+    # print(t1.complexes)
 
-    tube_results
+    tube_results = tube_analysis(tubes=[t1], model=my_model)
+    #
+    print(tube_results)
+
     pass
+
--- analysis/views.py
+++ analysis/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.generic import View
+
+from analysis.tool.analysis import analysis_all
 from analysis.tool.cal_tm import cal
 
 
@@ -24,9 +26,18 @@ class HomeView(View):
     def post(self, request):
         data = request.POST
         gene = data.get('gene_input')
+        temp = data.get('temperature')
+        Na = data.get('Na')
+
+        # var = request.FILES['gene_file']
+
+        # print(temp, Na)
+
         d1, d2 = cal(gene)
+        analysis_all(d1, d2)
 
         context = {
+            'gene_len': len(gene),
             'gene': gene,
             'd1': d1,
             'd2': d2,
